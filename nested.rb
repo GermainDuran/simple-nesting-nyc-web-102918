1b8dfdfad0eb7f5496abb85f51f4c2734ceeaa04
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-17 20:21:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -122,7 +122,8 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C","Assembly"]
+          
         }
      }
 
