05e9b8b6bca49ec187324a04e612bef825977acf
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-17 20:18:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -63,17 +63,17 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
+ 
 
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+ 		{     :yukihiro_matsumoto => {
+              :known_for => "Ruby",
+              :languages => ["LISP", "C"]
+          },
+          :grace_hopper => {
+              :known_for => "COBOL",
+              :languages => ["COBOL", "FORTRAN"]
+          },
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
